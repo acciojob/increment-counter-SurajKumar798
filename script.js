3c101898c0a6e66e5b8b9d8812125272df847609
SurajKumar798 <128129426+SurajKumar798@users.noreply.github.com>
2025-05-22 00:36:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const counter = document.getElementById('counter');
+const btn = document.getElementById('incrementBtn');
+incrementBtn.addEventListener('click', function(){
+	const currentValue = parseInt(counter.textContent);
+	alert(currentValue);
+	counter.textContent = currentValue + 1;
+})
